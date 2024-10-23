f5865fe016473664277a483ba8713b76652e6c4d
ha23039 <ha23039@ues.edu.sv>
2024-10-22 23:19:28 -0600

    Ejemplo de resolver conflicto en index.js miembro-c

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,3 @@
 document.getElementById("boton").addEventListener("click", function() {
-	document.getElementById("descripcion").textContent = "¡El botón ha sido presionado!";
+    document.getElementById("descripcion").textContent = "¡El botón ha sido presionado!";
 });
